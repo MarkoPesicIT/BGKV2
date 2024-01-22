b3603e96cfcd5ae9a2e26d2725d444b0efca72e9
KrticaTheMadLad <100162583+KrticaTheMadLad@users.noreply.github.com>
2024-01-22 23:49:10 +0100

    #123

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+      const images = document.querySelectorAll(".img");
+    
+      function clearActiveImage() {
+        images.forEach(function (image) {
+          image.classList.remove("active");
+        });
+      }
+    
+      images.forEach(function (image, index) {
+        image.onclick = function (event) {
+          event.stopPropagation();
+          if (images[index].classList.contains("active")) {
+            images[index].classList.remove("active");
+          } else {
+            clearActiveImage();
+            images[index].classList.add("active");
+          }
+        };
+      });
+    
+      document.addEventListener("click", function (event) {
+        clearActiveImage();
+      });
+    });
